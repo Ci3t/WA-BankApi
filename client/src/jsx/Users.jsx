5d897cd9d9a64fbe79e66e53055db7331c9c1c16
Ci3t <ranigfx@gmail.com>
2022-12-10 23:56:10 +0200

    frontendChanges

--- client/src/jsx/Users.jsx
+++ client/src/jsx/Users.jsx
@@ -6,11 +6,18 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Link } from 'react-router-dom';
+
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
 import './users.css'
 
 function Users() {
 
     const [getUsers,setGetUsers] = useState([])
+    const [firstNameInp,setFirstNameInp] = useState('')
+    const [lastNameInp,setLastNameInp] = useState('')
+    const [errorMsg,setErrorMsg] = useState('')
 
     useEffect(()=>{
         const fetchUsers = async ()=>{
@@ -19,12 +26,52 @@ function Users() {
             setGetUsers(data)
         }
         fetchUsers()
-    },[])
+    },[getUsers])
+    const createUser =async()=>{
+          
+      try{
+
+        await axios.post(`https://bankapi-s007.onrender.com/users`,{
+          firstName:firstNameInp,
+          lastName:lastNameInp
+        })
+      }catch(e){
+        setErrorMsg(e);
+      }
+
+    }
   return (
     <div className='containerusers'>
           <h1>User Accounts</h1>
+       <InputGroup id='maxWidth2' className="mb-3">
+        <InputGroup.Text id="inputGroup-sizing-default">
+        
+          FirstName
+        </InputGroup.Text>
+        <Form.Control
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        type={'text'}
+        value={firstNameInp}
+        onChange={(e)=>{setFirstNameInp(e.target.value)}}
+        />
+          <InputGroup.Text id="inputGroup-sizing-default">
+        
+        LastName
+      </InputGroup.Text>
+        <Form.Control
+          aria-label="Default"
+          aria-describedby="inputGroup-sizing-default"
+        type={'text'}
+        value={lastNameInp}
+        onChange={(e)=>{setLastNameInp(e.target.value)}}
+        />
+        <Button onClick={createUser} variant="primary">Create User</Button>
+      </InputGroup>
+      <div>
+        {errorMsg && errorMsg?.response.data}
+      </div>
         <div className='container-card'>
-       
       
        {getUsers.map(user=>(
         <React.Fragment key={user.passportId}>
